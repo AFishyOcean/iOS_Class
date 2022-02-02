ed0bc5252a79f5d76c7c036434081b37ef2ae3c2
David Jones <jonesda@ssfs.org>
2022-02-02 11:27:45 -0500

    added button

--- Hello World/Hello World/ViewController.swift
+++ Hello World/Hello World/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeText(_ sender: UIButton) {
+        helloLabel.text = "Hello User"
+    }
+    
 }
 
