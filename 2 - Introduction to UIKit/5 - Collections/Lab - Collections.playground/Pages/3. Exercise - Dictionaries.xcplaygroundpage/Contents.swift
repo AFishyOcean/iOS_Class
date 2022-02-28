fc8cf0d9bf571e2a36cf09c5955fe5111f1983fe
David Jones <jonesda@ssfs.org>
2022-02-28 12:10:29 -0500

    on last part of page 3

--- 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -3,24 +3,30 @@
 
  Create a variable `[String: Int]` dictionary that can be used to look up the number of days in a particular month. Use a dictionary literal to initialize it with January, February, and March. January contains 31 days, February has 28, and March has 31. Print the dictionary.
  */
-// var daysInMonths = ["January": 30, "February": 28, "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31, "September": 30, "October": 31, "November": 30, "December": 31]
-var daysInMonths = [String: Int]()
+var daysInMonths = ["January": 31, "February": 28, "March": 31]
 print(daysInMonths)
 
 //:  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
-
+daysInMonths["April"] = 30
 
 //:  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
-
-
+daysInMonths["February"] = 29
+print(daysInMonths)
 //:  Use if-let syntax to retrieve the number of days under "January." If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
+
+if let january = daysInMonths["January"] {
+    print("January has \(january) days")
+}
+    
 
 
 //:  Given the following arrays, create a new [String : [String]] dictionary. `shapesArray` should use the key "Shapes" and `colorsArray` should use the key "Colors." Print the resulting dictionary.
-
-
+var shapesArray = ["Square", "Circle", "Triangle"]
+var coloursArray = ["Red", "Green", "Blue"]
+var shapesAndColours = ["shapes": shapesArray, "colours": coloursArray]
+print(shapesAndColours)
 //:  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the force unwrap operator to unwrap what is returned from the dictionary before you can access an element of the array.
-
+print(shapesAndColours[coloursArray.endIndex])
 
 /*:
 [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
