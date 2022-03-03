6b10a118bc8d7aba74e7b48dad404180751a3390
David Jones <jonesda@ssfs.org>
2022-03-03 08:59:54 -0500

    on add size and buttons

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by David Jones on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
