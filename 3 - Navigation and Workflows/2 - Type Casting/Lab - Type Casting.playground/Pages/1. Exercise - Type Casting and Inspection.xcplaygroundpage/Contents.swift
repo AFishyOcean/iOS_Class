a798d78d314b506ef407f4f9547199008e7ac455
David Jones <jonesda@ssfs.org>
2022-04-08 15:15:13 -0400

    worked on pg 1

--- 3 - Navigation and Workflows/2 - Type Casting/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/2 - Type Casting/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
@@ -3,19 +3,29 @@
 
  Create a collection of type [Any], including a few doubles, integers, strings, and booleans within the collection. Print the contents of the collection.
  */
-var collection: [Any] = [5, "jane", 4.5]
+var collection: [Any] = [5, "jane", 4.5, true]
 print(collection)
 //:  Loop through the collection. For each integer, print "The integer has a value of ", followed by the integer value. Repeat the steps for doubles, strings and booleans.
 for part in collection {
-    if
+    if part is Int {
+        print("The integer value of collection is \(part)")
+    } else if part is Double {
+        print("The double value of collection is \(part)")
+    } else if part is String {
+        print("The string of collections is \(part)")
+    } else {
+        print("\(part) is not a integer, double, or string")
+    }
 }
 
 //:  Create a [String : Any] dictionary, where the values are a mixture of doubles, integers, strings, and booleans. Print the key/value pairs within the collection
- 
-
+var things: [String : Any] = ["money": 50, "interest": true, "debt": 1.50]
+print(things)
 //:  Create a variable `total` of type `Double` set to 0. Then loop through the dictionary, and add the value of each integer and double to your variable's value. For each string value, add 1 to the total. For each boolean, add 2 to the total if the boolean is `true`, or subtract 3 if it's `false`. Print the value of `total`.
- 
-
+var total: Double = 0
+for size in things {
+    if size 
+}
 //:  Create a variable `total2` of type `Double` set to 0. Loop through the collection again, adding up all the integers and doubles. For each string that you come across during the loop, attempt to convert the string into a number, and add that value to the total. Ignore booleans. Print the total.
  
 
