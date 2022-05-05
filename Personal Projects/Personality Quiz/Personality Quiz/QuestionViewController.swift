358ac7be04fb1c99c87c064f1e53606d2b3d4e77
David Jones <jonesda@ssfs.org>
2022-05-05 10:59:41 -0400

    worked on code

--- Personal Projects/Personality Quiz/Personality Quiz/QuestionViewController.swift
+++ Personal Projects/Personality Quiz/Personality Quiz/QuestionViewController.swift
@@ -8,22 +8,68 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
+        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet weak var singleStackView: UIStackView!
+    var questions: [Question] = [
+      Question(
+        text: "Which food do you like the most?",
+        type: .single,
+        answers: [
+          Answer(text: "Steak", type: .dog),
+          Answer(text: "Fish", type: .cat),
+          Answer(text: "Carrots", type: .rabbit),
+          Answer(text: "Corn", type: .turtle)
+        ]
+      ),
 
+      Question(
+        text: "Which activities do you enjoy?",
+        type: .multiple,
+        answers: [
+          Answer(text: "Swimming", type: .turtle),
+          Answer(text: "Sleeping", type: .cat),
+          Answer(text: "Cuddling", type: .rabbit),
+          Answer(text: "Eating", type: .dog)
+        ]
+      ),
+
+      Question(
+        text: "How much do you enjoy car rides?",
+        type: .ranged,
+        answers: [
+          Answer(text: "I dislike them", type: .cat),
+          Answer(text: "I get a little nervous", type: .rabbit),
+          Answer(text: "I barely notice them", type: .turtle),
+          Answer(text: "I love them", type: .dog)
+        ]
+      )
+    ]
+
+    var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-    
+    func updateUI() {
+        singleStackView.isHidden = true
+        multipleStackView.isHidden = true
+        rangedStackView.isHidden = true
 
-    /*
-    // MARK: - Navigation
+        navigationItem.title = "Question #\(questionIndex + 1)"
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let currentQuestion = questions[questionIndex]
+
+        switch currentQuestion.type {
+        case .single:
+            singleStackView.isHidden = false
+        case .multiple:
+            multipleStackView.isHidden = false
+        case .ranged:
+            rangedStackView.isHidden = false
+        }
     }
-    */
-
 }
