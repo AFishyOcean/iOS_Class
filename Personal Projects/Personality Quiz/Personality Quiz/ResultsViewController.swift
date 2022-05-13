f0aa7026546070ab0808aec9904d777964683100
David Jones <jonesda@ssfs.org>
2022-05-13 09:46:06 -0400

    finished

--- Personal Projects/Personality Quiz/Personality Quiz/ResultsViewController.swift
+++ Personal Projects/Personality Quiz/Personality Quiz/ResultsViewController.swift
@@ -10,7 +10,9 @@ import UIKit
 
 
 class ResultsViewController: UIViewController {
-
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    
     var responses: [Answer]
     init?(coder:NSCoder, responses: [Answer]) {
         self.responses = responses
@@ -23,13 +25,18 @@ class ResultsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
         calculatePersonalityResult()
     }
     func calculatePersonalityResult() {
         let frequencyOfAnswers = responses.reduce(into: [:]) { (counts, answer) in
             counts[answer.type, default: 0] += 1
         }
-        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $ 1.1 }.first!.key
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.definition
     }
     
 }
