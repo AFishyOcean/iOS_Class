d6a9698ea47bf38b12ad06d790743ae4a1fbb83f
David Jones <jonesda@ssfs.org>
2022-05-24 09:55:00 -0400

    worked on timer

--- Personalized Pomodoro/Personalized Pomodoro/ViewController.swift
+++ Personalized Pomodoro/Personalized Pomodoro/ViewController.swift
@@ -9,6 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var OurTimer = Timer()
+    var TimerDisplayed = 0
+    var InputWorkTime = 10
+    
+    @IBOutlet weak var Label: UILabel!
+    
+
+    @IBAction func StartBTN(_ sender: Any) {
+        TimerDisplayed = InputWorkTime
+        Label.text = String(TimerDisplayed)
+        OurTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+        StartBTN
+    }
+    
+    @IBAction func PauseBTN(_ sender: Any) {
+        OurTimer.invalidate()
+    }
+    
+    @IBAction func ResetBTN(_ sender: Any) {
+        OurTimer.invalidate()
+        TimerDisplayed = 0
+        Label.text = "0"
+    }
+    
+    @objc func Action(){
+        TimerDisplayed -= 1
+        if TimerDisplayed <= 0{
+            OurTimer.invalidate()
+            Label.text = "0"
+        }
+        Label.text = String(TimerDisplayed)
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
